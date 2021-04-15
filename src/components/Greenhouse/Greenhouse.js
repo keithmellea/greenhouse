aa6022600ec0a223501f2926d7e29bf9d7047b4f
Keith Mellea <keithmellea@gmail.com>
2021-04-15 13:46:33 -0400

    phase 2 done

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,15 +1,18 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
+import { useTheme } from '../../context/ThemeContext';
 
 import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
-
 function Greenhouse() {
+const { themeName, setThemeName } = useTheme();
+const themeImg = themeName === "day" ? dayImage : nightImage;
+console.log(themeImg);
   return (
     <section>
-      <img className='greenhouse-img' src={dayImage} alt='greenhouse' />
+      <img className='greenhouse-img' src={themeImg} alt='greenhouse' />
       <LightSwitch />
       <ClimateStats />
     </section>
--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,12 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const { themeName, setThemeName} = useTheme();
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={() => {setThemeName('day')}}>DAY</div>
+      <div className="off" onClick={() => {setThemeName('night')}}>NIGHT</div>
     </div>
   );
 }
